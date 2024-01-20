275d71ed079f3a1c9d48ff77d764ec43b72fa2ba
Deyan_Kavindu <87009309+Deyankavindu@users.noreply.github.com>
2024-01-20 17:26:15 +0530

    first commit

---
+++ Backend/routes/user.js
@@ -0,0 +1,9 @@
+const express =require('express');
+const connection = require('../connection');
+const { route } = require('..');
+const router = express.Router();
+
+router.post
+
+
+module.exports = router;
